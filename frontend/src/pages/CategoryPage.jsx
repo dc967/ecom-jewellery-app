68083c6bc8338ac3b65707a2f1929da2f8fe9732
dc967 <dc3879643@gmail.com>
2026-07-20 11:03:56 +0530

    Add LoginPage with authService and show/hide password feature

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 import { getAllItems } from "../services/itemService";
 
 function CategoryPage(){
-    const { CategoryName } = useParams();
+    const { categoryName } = useParams();
     const [items, setItems] = useState([]);
     const [loading, setLoding] = useState(true);
 
     useEffect(() => {
         const fetchItems = async () => {
             try {
-                const data = await getAllItems(CategoryName);
+                const data = await getAllItems(categoryName);
                 setItems(data.items);
             } catch (error) {
                  console.log('Failed to fetch items', error);
@@ -20,13 +20,13 @@ function CategoryPage(){
             }
         };
         fetchItems();
-    }, [CategoryName]);
+    }, [categoryName]);
 
     return (
 
       <div className="px-6 py-10">
         <h1 className="text-3xl font-bold text-center capitalize">
-            {CategoryName} 
+            {categoryName} 
         </h1>
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,106 @@
-
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../services/authService";
+import { Eye, EyeOff } from 'lucide-react';
 
 function LoginPage(){
-    return <h1 className="text-3xl font-bold text-center mt-10">Login Page</h1>
+   const [email, serEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [error, setError] = useState('');
+   const navigate = useNavigate();
+   const [showPassword, setShowPassword] = useState(false);
+
+   const handleSubmit = async (e) => {
+     e.preventDefault();
+     setError('');
+
+     try {
+        const data = await loginUser(email, password);
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate('/');
+     } catch (error) {
+        setError(err.response?.data?.msg || 'Login failed');
+     }
+   };
+
+   return (
+
+       <div className="flex justify-center items-center min-h-[80vh] px-4">
+         <form 
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded-lg p-8 w-full max-w-sm"
+         >
+           
+           <h1 className="text-2xl font-bold text-center text-pink-600 mb-6">
+            Login
+           </h1>
+
+            {error && (
+                <p className="text-red-500 text-sm text-center mb-4">{error}</p>
+            )}
+
+            <label className="block text-gray-700 mb-1">Email</label>
+            <input 
+                type="email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full border rounded px-3 py-2 mb-4 focus:outline-none focus:ring-2 focus:ring-pink-400"
+            />
+
+
+            <label className="block text-gray-700 mb-1">Password</label>
+               <div className="relative mb-6">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className="w-full border rounded px-3 py-2 pr-10 focus:outline-none focus:ring-2 focus:ring-pink-400"
+                    />
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+                    >
+                        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </button>
+                </div>
+
+           <button
+                 type="submit"
+                 className="w-full bg-pink-600 text-white py-2 rounded hover:bg-pink-700 transition"
+           >
+              Login
+           </button>
+         </form>
+       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+   )
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 export default LoginPage
